4bf87231092e22f119e2d1980261e6c042db23a9
thm <thasnim.hm@gmail.com>
2021-11-10 22:27:15 +0530

    package change

--- src/main/java/link/thasnim/DiscoveryServerApplication.java
+++ src/main/java/link/thasnim/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package io.schultz.dustin;
+package link.thasnim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/link/thasnim/DiscoveryServerApplicationTests.java
+++ src/test/java/link/thasnim/DiscoveryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.schultz.dustin;
+package link.thasnim;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
